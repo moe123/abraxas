f3526a99d065813bff5c7acbb326bf30cd7ea98e
moe123 <moe@aermob.com>
2017-01-19 18:44:14 -0800

    iostream, set

---
+++ scl/bits/scl_iostream.php
@@ -0,0 +1,24 @@
+<?php
+# -*- coding: utf-8 -*-
+
+//
+// scl_iostream.php
+//
+// Copyright (C) 2017 Moe123. All rights reserved.
+//
+ 
+/*! 
+ * @author     Moe123 2017.
+ * @maintainer Moe123 2017.
+ *
+ * @copyright  (C) Moe123. All rights reserved.
+ */
+
+namespace
+{
+	require_once __DIR__ . DIRECTORY_SEPARATOR . "scl_base_stdio.php";
+	require_once __DIR__ . DIRECTORY_SEPARATOR . "scl_base_stdlib.php";
+	require_once __DIR__ . DIRECTORY_SEPARATOR . "scl_basic_ios.php";
+} /* EONS */
+
+/* EOF */
