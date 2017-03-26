b51a8cf95b435b8456b05db8758d32268b8a10e1
moe123 <moe@aermob.com>
2017-03-26 10:10:27 -0700

    x complex

--- scl/_b/_x/xcomplex.php
+++ scl/_b/_x/xcomplex.php
@@ -32,131 +32,131 @@ namespace std
 	function newcomplex(float $r___, float $i___)
 	{ return new complex_t($r___, $i___); }
 
-	function ccos(complex_t $x___)
+	function ccos(complex_t $z___)
 	{
 		return newcomplex(
-			  \cos($x___->_M_real) * \cosh($x___->_M_imag)
-			, \sin($x___->_M_real) * \sinh($x___->_M_imag)
+			  \cos($z___->_M_real) * \cosh($z___->_M_imag)
+			, \sin($z___->_M_real) * \sinh($z___->_M_imag)
 		);
 	}
 
-	function csin(complex_t $x___)
+	function csin(complex_t $z___)
 	{
 		return newcomplex(
-			  \sin($x___->_M_real) * \cosh($x___->_M_imag)
-			, \cos($x___->_M_real) * \sinh($x___->_M_imag)
+			  \sin($z___->_M_real) * \cosh($z___->_M_imag)
+			, \cos($z___->_M_real) * \sinh($z___->_M_imag)
 		);
 	}
 
-	function ctan(complex_t $x___)
+	function ctan(complex_t $z___)
 	{
-		$d = 1 + \pow(\tan($x___->_M_real), 2) * \pow(\tanh($x___->_M_imag), 2);
+		$d = 1 + \pow(\tan($z___->_M_real), 2) * \pow(\tanh($z___->_M_imag), 2);
 		return newcomplex(
-			  \pow((1 / \cosh($x___->_M_imag)), 2) * \tan($x___->_M_real) / $d
-			, \pow((1 / \cos($x___->_M_real)), 2) * \tanh($x___->_M_imag) / $d
+			  \pow((1 / \cosh($z___->_M_imag)), 2) * \tan($z___->_M_real) / $d
+			, \pow((1 / \cos($z___->_M_real)), 2) * \tanh($z___->_M_imag) / $d
 		);
 	}
 
-	function ccosh(complex_t $x___)
+	function ccosh(complex_t $z___)
 	{
 		return newcomplex(
-			  \cosh($x___->_M_real) * \cos($x___->_M_imag)
-			, \sinh($x___->_M_real) * \sin($x___->_M_imag)
+			  \cosh($z___->_M_real) * \cos($z___->_M_imag)
+			, \sinh($z___->_M_real) * \sin($z___->_M_imag)
 		);
 	}
 
-	function csinh(complex_t $x___)
+	function csinh(complex_t $z___)
 	{
 		return newcomplex(
-			  \sinh($x___->_M_real) * \cos($x___->_M_imag)
-			, \cosh($x___->_M_real) * \sin($x___->_M_imag)
+			  \sinh($z___->_M_real) * \cos($z___->_M_imag)
+			, \cosh($z___->_M_real) * \sin($z___->_M_imag)
 		);
 	}
 
-	function ctanh(complex_t $x___)
+	function ctanh(complex_t $z___)
 	{
-		$d = \cos($x___->_M_imag) * \cos($x___->_M_imag) + \sinh($x___->_M_real) * \sinh($x___->_M_real);
+		$d = \cos($z___->_M_imag) * \cos($z___->_M_imag) + \sinh($z___->_M_real) * \sinh($z___->_M_real);
 		return newcomplex(
-			  \sinh($x___->_M_real) * \cosh($x___->_M_real) / $d
-			, 0.5 * \sin(2 * $x___->_M_imag) / $d
+			  \sinh($z___->_M_real) * \cosh($z___->_M_real) / $d
+			, 0.5 * \sin(2 * $z___->_M_imag) / $d
 		);
 	}
 
-	function cexp(complex_t $x___)
+	function cexp(complex_t $z___)
 	{
 		return newcomplex(
-			  \exp($x___->_M_real) * \cos($x___->_M_imag)
-			, \exp($x___->_M_real) * \sin($x___->_M_imag)
+			  \exp($z___->_M_real) * \cos($z___->_M_imag)
+			, \exp($z___->_M_real) * \sin($z___->_M_imag)
 		);
 	}
 
-	function clog(complex_t $x___)
+	function clog(complex_t $z___)
 	{
 		return newcomplex(
-			  \log(\sqrt($x___->_M_real * $x___->_M_real + $x___->_M_imag * $x___->_M_imag))
-			, \atan2($x___->_M_imag, $x___->_M_real)
+			  \log(\sqrt($z___->_M_real * $z___->_M_real + $z___->_M_imag * $z___->_M_imag))
+			, \atan2($z___->_M_imag, $z___->_M_real)
 		);
 	}
 
-	function cabs(complex_t $x___)
-	{ return \sqrt($x___->_M_real * $x___->_M_real + $x___->_M_imag * $x___->_M_imag); }
+	function cabs(complex_t $z___)
+	{ return \sqrt($z___->_M_real * $z___->_M_real + $z___->_M_imag * $z___->_M_imag); }
 
-	function carg(complex_t $x___)
-	{ return \atan2($x___->_M_imag, $x___->_M_real); }
+	function carg(complex_t $z___)
+	{ return \atan2($z___->_M_imag, $z___->_M_real); }
 
-	function cpow(complex_t $x1___, complex_t $x2___) 
+	function cpow(complex_t $z1___, complex_t $z2___) 
 	{
-		if (_X_real_zeroed($x1___->_M_real, $x1___->_M_imag)) {
+		if (_X_real_zeroed($z1___->_M_real, $z1___->_M_imag)) {
 			return $x1;
 		}
 
-		$l = \log(\sqrt($x1___->_M_real * $x1___->_M_real + $x1___->_M_imag * $x1___->_M_imag));
-		$T = \atan2($x1___->_M_imag, $x1___->_M_real);
-		$R = \exp($l * $x2___->_M_real - $x2___->_M_imag * $T);
-		$B = $T * $x2___->_M_real + $x2___->_M_imag * $l;
+		$l = \log(\sqrt($z1___->_M_real * $z1___->_M_real + $z1___->_M_imag * $z1___->_M_imag));
+		$T = \atan2($z1___->_M_imag, $z1___->_M_real);
+		$R = \exp($l * $z2___->_M_real - $z2___->_M_imag * $T);
+		$B = $T * $z2___->_M_real + $z2___->_M_imag * $l;
 
 		return newcomplex($R * \cos($B), $R * \sin($B));
 	}
 
-	function csqrt(complex_t $x___) 
+	function csqrt(complex_t $z___) 
 	{
-		if (_X_real_zeroed($x___->_M_real, $x___->_M_imag)) {
+		if (_X_real_zeroed($z___->_M_real, $z___->_M_imag)) {
 			return $x;
 		}
 
-		$a = \abs($x___->_M_real);
-		$b = \abs($x___->_M_imag);
+		$a = \abs($z___->_M_real);
+		$b = \abs($z___->_M_imag);
 
 		$W = (($a >= $b)
 			? \sqrt($a) * \sqrt(0.5 * (1.0 + \sqrt(1.0 + ($b / $a) * ($b / $a))))
 			: \sqrt($b) * \sqrt(0.5 * (($a / $b) + \sqrt(1.0 + ($a / $b) * ($a / $b))))
 		);
 
-		if ($x___->_M_real > 0.0 || _X_real_iszero($x___->_M_real)) {
+		if ($z___->_M_real > 0.0 || _X_real_iszero($z___->_M_real)) {
 			return newcomplex(
 				  $W
-				, $x___->_M_imag / (2.0 * $W)
+				, $z___->_M_imag / (2.0 * $W)
 			);
 		}
 
-		$I = ($x___->_M_imag >= 0) ? $W : -1 * $W;
+		$I = ($z___->_M_imag >= 0) ? $W : -1 * $W;
 		return newcomplex(
-			$x___->_M_imag / (2.0 * $I)
+			$z___->_M_imag / (2.0 * $I)
 			, $I
 		);
 	}
 
-	function cconj(complex_t $x___)
+	function cconj(complex_t $z___)
 	{
 		return newcomplex(
-			  $x___->_M_real
-			, $x___->_M_imag * -1
+			  $z___->_M_real
+			, $z___->_M_imag * -1
 		);
 	}
 
-	function cproj(complex_t $x___)
+	function cproj(complex_t $z___)
 	{
-		$z = clone $x___;
+		$z = clone $z___;
 		if (isinf($z->_M_real) || isinf($z->_M_imag)) {
 			$z->_M_real = \INF;
 			$z->_M_imag = copysign(0.0, $z->_M_imag);
@@ -164,11 +164,11 @@ namespace std
 		return $z;
 	}
 
-	function creal(complex_t $x___)
-	{ return $x___->_M_real; }
+	function creal(complex_t $z___)
+	{ return $z___->_M_real; }
 
-	function cimag(complex_t $x___)
-	{ return $x___->_M_imag; }
+	function cimag(complex_t $z___)
+	{ return $z___->_M_imag; }
 } /* EONS */
 
 /* EOF */
