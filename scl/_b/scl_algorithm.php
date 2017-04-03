ecdde3511b29ef072535ffe698a24b24faf403c9
moe123 <moe@aermob.com>
2017-04-03 10:25:40 -0700

    moving swaping parts

--- scl/_b/scl_algorithm.php
+++ scl/_b/scl_algorithm.php
@@ -1583,6 +1583,169 @@ namespace std
 		, basic_iterator $last___
 		, callable       $compare___ = null
 	) { _F_sort_r($first___, $last___, $compare___); }
+
+	function median(
+		  basic_iterator $first___
+		, basic_iterator $last___
+	) {
+		if ($first___::iterator_category === $last___::iterator_category) {
+			if (1 < ($dist = distance($first___, $last___))) {
+				$med  = 0.0;
+				if (($dist % 2) == 0) {
+					$med  = iter_value_at_position($first___, $dist / 2);
+					$med += iter_value_at_position($first___, (($dist / 2) - 1));
+					$med /= 2.0;
+				} else {
+					$med = iter_value_at_position($first___, $dist / 2);
+				}
+				return $med;
+			}
+			return 0.0;
+		} else {
+			_F_throw_invalid_argument("Invalid type error");
+		}
+		return \NAN;
+	}
+
+	function mode(
+		  basic_iterator $first___
+		, basic_iterator $last___
+	) {
+		if ($first___::iterator_category === $last___::iterator_category) {
+			$dist = distance($first___, $last___);
+			if (0 < $dist) {
+				$it = clone $first___;
+				return max_element(
+					  $first___
+					, $last___
+					, function ($v) use($it, $last___) {
+						return count(clone $it, $last___, $v);
+					}
+				);
+			}
+		} else {
+			_F_throw_invalid_argument("Invalid type error");
+		}
+		return \NAN;
+	}
+
+	function mid_range(
+		  basic_iterator $first___
+		, basic_iterator $last___
+	) {
+		if ($first___::iterator_category === $last___::iterator_category) {
+			$largest  = max_element(clone $first___, $last___);
+			$smallest = min_element($first___, $last___);
+			return ($largest->_F_this() - $smallest->_F_this());
+		} else {
+			_F_throw_invalid_argument("Invalid type error");
+		}
+		return \NAN;
+	}
+
+	function mean(
+		  basic_iterator $first___
+		, basic_iterator $last___
+	) {
+		if ($first___::iterator_category === $last___::iterator_category) {
+			$dist = distance($first___, $last___);
+			if (0 < $dist) {
+				$sum  = 0.0;
+				while ($first___ != $last___) {
+					$sum += $first___->_F_this();
+					$first___->_F_next();
+				}
+				return $sum / $dist;
+			}
+			return 0.0;
+		} else {
+			_F_throw_invalid_argument("Invalid type error");
+		}
+		return \NAN;
+	}
+
+	function summation(
+		  basic_iterator $first___
+		, basic_iterator $last___
+	) {
+		if ($first___::iterator_category === $last___::iterator_category) {
+			$sum  = 0.0;
+			while ($first___ != $last___) {
+				$sum += $first___->_F_this();
+				$first___->_F_next();
+			}
+			return $sum;
+		} else {
+			_F_throw_invalid_argument("Invalid type error");
+		}
+		return \NAN;
+	}
+
+	function variance(
+		  basic_iterator $first___
+		, basic_iterator $last___
+		, bool           $unbiased___ = true
+	) {
+		if ($first___::iterator_category === $last___::iterator_category) {
+			$dist = distance($first___, $last___);
+			if (0 < $dist) {
+				$mean = mean((clone $first___), $last___);
+				$sum  = 0.0;
+				while ($first___ != $last___) {
+					$sum += \pow($first___->_F_this() - $mean, 2);
+					$first___->_F_next();
+				}
+				if (1 < $dist && $unbiased___) {
+					return ($sum / ($dist - 1));
+				}
+				return ($sum / $dist);
+			}
+			return 0.0;
+		} else {
+			_F_throw_invalid_argument("Invalid type error");
+		}
+		return \NAN;
+	}
+
+	function covariance(
+		  basic_iterator $first1___
+		, basic_iterator $last1___
+		, basic_iterator $first2___
+		, basic_iterator $last2___
+		, bool           $unbiased___ = true
+	) {
+		if (
+			$first1___::iterator_category === $last1___::iterator_category &&
+			$first2___::iterator_category === $last2___::iterator_category
+		) {
+			$dist  = max(
+				  distance($first1___, $last1___)
+				, distance($first2___, $last2___)
+			);
+			if (0 < $dist) {
+				$mean1 = mean((clone $first1___), $last1___);
+				$mean2 = mean((clone $first2___), $last2___);
+				$sum   = 0.0;
+				while ($first1___ != $last1___ && $first2___ != $last2___) {
+					$sum += ($first1___->_F_this() - $mean1) * ($first2___->_F_this() - $mean2);
+				}
+				if (1 < $dist && $unbiased___) {
+					return ($sum / ($dist - 1));
+				}
+				return ($sum / $dist);
+			}
+			return 0.0;
+		} else {
+			_F_throw_invalid_argument("Invalid type error");
+		}
+		return \NAN;
+	}
+
+	function standard_deviation(
+		  basic_iterator $first___
+		, basic_iterator $last___
+		, bool           $unbiased___ = true
+	) { return \sqrt(variance($first___, $last___, $unbiased___)); }
 } /* EONS */
 
 /* EOF */
